3152e5a6bb024f04b293d6bed8c0134e6b25d4c6
Nasib Bartaula <bartaulanasib9@gmail.com>
2026-03-14 22:33:55 +0545

    added sql queries

--- sql/practice_queries.sql
+++ sql/practice_queries.sql
@@ -63,7 +63,75 @@ select * from sakila.film_list
 except
 select * from sakila.nicer_but_slower_film_list;
 
+select * from sakila.payment;
+select avg(amount) from sakila.payment;
 
+select * from sakila.payment where amount >=4.2;
+
+select * from sakila.payment where amount >
+(select avg(amount) from sakila.payment);
+
+select * from sakila.actor
+where first_name in (select staff.first_name from sakila.staff);
+
+select * from sakila.actor
+where first_name not in (select staff.first_name from sakila.staff);
+
+create view total as
+select customer_id , sum(amount) from sakila.payment
+group by customer_id;
+
+select * from total;
+
+delimiter &&
+create procedure get_it()
+begin
+	select * from sakila.payment where amount >=4.2;
+end &&
+delimiter ;
+call get_it();    
+
+delimiter &&
+create procedure get_limit(in var int)
+begin
+	select * from sakila.payment limit var;
+end &&
+delimiter ;
+call get_limit(3);    
+
+delimiter &&
+create procedure get_credit(out var int)
+begin
+	select max(amount) into var from sakila.payment;
+end &&
+delimiter ;
+call get_credit(@abc);
+select @abc;   
+
+delimiter &&
+create procedure get_name(inout var int)
+begin
+	select payment.amount from sakila.payment where customer_id = var;
+end &&
+delimiter ;
+set @m=2;
+call get_name(@m);   
+	
+select customer_id, SUM(amount) as total_payment
+from sakila.payment
+group by customer_id;    
+
+select customer_id, rental_id, amount
+from sakila.payment
+order by amount desc
+limit 5;
+
+select date(payment_date) as payment_day,
+       SUM(amount) as daily_revenue
+from sakila.payment
+group by payment_day
+order by payment_day;
+     
 
 
 
